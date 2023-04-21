53a442f3db018d42447787ac732249d23da6703b
AnthonyD77 <45114182+AnthonyD77@users.noreply.github.com>
2023-04-21 17:10:51 +0800

    Update graph_hash_of_mixed_weighted_binary_operations.h

--- graph_hash_of_mixed_weighted/graph_hash_of_mixed_weighted_binary_operations.h
+++ graph_hash_of_mixed_weighted/graph_hash_of_mixed_weighted_binary_operations.h
@@ -112,6 +112,32 @@ void graph_hash_of_mixed_weighted_binary_operations_erase(std::vector<std::pair<
 
 }
 
+template <typename T>
+void graph_hash_of_mixed_weighted_binary_operations_erase_infty(std::vector<std::pair<int, T>>& input_vector, int key) {
+
+	/*erase key from vector; time complexity O(log n + size()-position ), which is O(n) in the worst case, as
+	the time complexity of erasing an element from a vector is the number of elements behind this element*/
+
+	if (input_vector.size() > 0) {
+		int left = 0, right = input_vector.size() - 1;
+
+		while (left <= right) {
+			int mid = left + ((right - left) / 2);
+			if (input_vector[mid].first == key && input_vector[mid].second == std::numeric_limits<T>::max()) {
+				input_vector.erase(input_vector.begin() + mid);
+				break;
+			}
+			else if (input_vector[mid].first > key) {
+				right = mid - 1;
+			}
+			else {
+				left = mid + 1;
+			}
+		}
+	}
+
+}
+
 
 template <typename T>
 int graph_hash_of_mixed_weighted_binary_operations_insert(std::vector<std::pair<int, T>>& input_vector, int key, T load) {
