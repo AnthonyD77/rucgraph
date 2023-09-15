19123079735ca02c28b446973ae0c1ef7c9e8cb5
rucdatascience <yahuisun@ruc.edu.cn>
2023-09-15 20:17:41 +0800

    1

--- build_in_progress/HL/dynamic/test_dynamic.h
+++ build_in_progress/HL/dynamic/test_dynamic.h
@@ -200,9 +200,9 @@ void graph_change_and_label_maintenance(graph_hash_of_mixed_weighted& instance_g
 			auto begin = std::chrono::high_resolution_clock::now();
 
 			/*maintain labels*/
-			// WeightIncrease2021(instance_graph, mm, selected_edge.first, selected_edge.second, selected_edge_weight);
-			// WeightIncreaseMaintenance_improv(instance_graph, mm, selected_edge.first, selected_edge.second, selected_edge_weight);
-			WeightIncrease2019(instance_graph, mm, selected_edge.first, selected_edge.second, selected_edge_weight);
+			//WeightIncrease2021(instance_graph, mm, selected_edge.first, selected_edge.second, selected_edge_weight);
+			//WeightIncreaseMaintenance_improv(instance_graph, mm, selected_edge.first, selected_edge.second, selected_edge_weight);
+			WeightIncrease2019(instance_graph, mm, selected_edge.first, selected_edge.second, selected_edge_weight, 1e2);
 			//cout << "1ec change " << selected_edge.first << " " << selected_edge.second << " " << selected_edge_weight * (1 + weightChange_ratio) << endl;
 			//mm.print_L();
 			//mm.print_PPR();
@@ -265,8 +265,8 @@ void graph_change_and_label_maintenance(graph_hash_of_mixed_weighted& instance_g
 			auto begin = std::chrono::high_resolution_clock::now();
 
 			/*maintain labels*/
-			// WeightDecreaseMaintenance_improv(instance_graph, mm, selected_edge.first, selected_edge.second, new_ec);
-			// WeightDecrease2021(instance_graph, mm, selected_edge.first, selected_edge.second, new_ec);
+			//WeightDecreaseMaintenance_improv(instance_graph, mm, selected_edge.first, selected_edge.second, new_ec);
+			//WeightDecrease2021(instance_graph, mm, selected_edge.first, selected_edge.second, new_ec);
 			WeightDecrease2014(instance_graph, mm, selected_edge.first, selected_edge.second, new_ec);
 			//cout << "2ec change " << selected_edge.first << " " << selected_edge.second << " " << selected_edge_weight * (1 - weightChange_ratio) << endl;
 			//mm.print_L();
