38ab4de452dab2106430f5c312f04a16e535ec90
rucdatascience <yahuisun@ruc.edu.cn>
2023-10-12 11:28:42 +0800

    Update exp.cpp

--- build_in_progress/HL/dynamic/exp.cpp
+++ build_in_progress/HL/dynamic/exp.cpp
@@ -415,8 +415,8 @@ void exp_element(string data_name, double weightChange_ratio, int change_times, 
 					PPR_bit_size_afterClean2 = mm.compute_PPR_bit_size();
 
 					mm.clear_labels();
-					graph_hash_of_mixed_weighted g = graph_v_of_v_idealID_to_graph_hash_of_mixed_weighted(instance_graph);
 					begin = std::chrono::high_resolution_clock::now();
+					graph_hash_of_mixed_weighted g = graph_v_of_v_idealID_to_graph_hash_of_mixed_weighted(instance_graph);				
 					PLL_dynamic(g, instance_graph.size() + 1, thread_num, mm);
 					clean_L_dynamic(mm.L, mm.PPR, pool_dynamic, results_dynamic, thread_num);
 					clean_PPR(instance_graph, mm.L, mm.PPR, pool_dynamic, results_dynamic, thread_num);
