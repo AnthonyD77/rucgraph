ef7d2f829edb9309f5932417f585416ec9bf4d43
YahuiSun <yahuisun@ruc.edu.cn>
2023-10-23 22:45:27 +0800

    Update test_dynamic.h

--- build_in_progress/HL/dynamic/test_dynamic.h
+++ build_in_progress/HL/dynamic/test_dynamic.h
@@ -271,10 +271,10 @@ void test_dynamic() {
 
 	/*parameters*/
 	int iteration_graph_times = 1e4, iteration_source_times = 10, iteration_terminal_times = 10;
-	int V = 100, E = 500, precision = 1, thread_num = 10;
+	int V = 100, E = 150, precision = 1, thread_num = 10;
 	double ec_min = 1, ec_max = 10;
 
-	int weightIncrease_time = 50, weightDecrease_time = 50;
+	int weightIncrease_time = 30, weightDecrease_time = 30;
 	double weightChange_ratio = 0.2;
 
 	double avg_index_time = 0, avg_index_size_per_v = 0, avg_maintain_time = 0;
