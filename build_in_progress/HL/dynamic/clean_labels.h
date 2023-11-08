676fc9549aca7e2c171b7875d379495bf6d3f372
rucdatascience <yahuisun@ruc.edu.cn>
2023-11-08 11:34:13 +0800

    Update clean_labels.h

--- build_in_progress/HL/dynamic/clean_labels.h
+++ build_in_progress/HL/dynamic/clean_labels.h
@@ -2,94 +2,102 @@
 #include <build_in_progress/HL/dynamic/two_hop_labels_base.h>
 
 
+/*
+in the following codes, it is faster to make each vertex a task in the thread pool, not a fixed vector of vertices as a task.
+
+a fixed vector of vertices as a task:
+there are imbalances between workloads of vectors of vertices, and most threads would wait for a few threads to finish in the end
+*/
 
 
 /*clean L*/
 
-vector<vector<int>> thread_vertices_clean_L_dynamic;
 vector<vector<double>> T_clean_L_dynamic;
+queue<int> Qid_clean_PPR;
 
 void clean_L_dynamic(vector<vector<two_hop_label_v1>>& L, PPR_type& PPR, ThreadPool& pool_dynamic, std::vector<std::future<int>>& results_dynamic, int thread_num) {
 
 	int N = L.size();
 
-	vector<vector<int>>().swap(thread_vertices_clean_L_dynamic);
-	thread_vertices_clean_L_dynamic.resize(thread_num);
-	for (int i = 0; i < N; i++) {
-		thread_vertices_clean_L_dynamic[i % thread_num].push_back(i);
-	}
 	vector<vector<double>>().swap(T_clean_L_dynamic);
 	T_clean_L_dynamic.resize(thread_num);
+	queue<int>().swap(Qid_clean_PPR);
 	for (int i = 0; i < thread_num; i++) {
 		T_clean_L_dynamic[i].resize(N, std::numeric_limits<weightTYPE>::max());
+		Qid_clean_PPR.push(i);
 	}
 
-	for (int thread_id = 0; thread_id < thread_num; thread_id++) {
+	for (int v = 0; v < N; v++) {
 		results_dynamic.emplace_back(
-			pool_dynamic.enqueue([thread_id, &L, &PPR] { // pass const type value j to thread; [] can be empty
+			pool_dynamic.enqueue([v, &L, &PPR] { // pass const type value j to thread; [] can be empty
 
-				for (auto v : thread_vertices_clean_L_dynamic[thread_id]) {
+				mtx_595[max_N_ID_for_mtx_595 - 1].lock();
+				int used_id = Qid_clean_PPR.front();
+				Qid_clean_PPR.pop();
+				mtx_595[max_N_ID_for_mtx_595 - 1].unlock();
 
-					vector<two_hop_label_v1> Lv_final;
+				vector<two_hop_label_v1> Lv_final;
 
-					mtx_595[v].lock();
-					vector<two_hop_label_v1> Lv = L[v];
-					mtx_595[v].unlock();
+				mtx_595[v].lock();
+				vector<two_hop_label_v1> Lv = L[v];
+				mtx_595[v].unlock();
 
-					auto& T = T_clean_L_dynamic[thread_id];
+				auto& T = T_clean_L_dynamic[used_id];
 
-					int size = Lv.size();
-					for (int i = 0; i < size; i++) {
-						int u = Lv[i].vertex;
-						if (Lv[i].distance == std::numeric_limits<weightTYPE>::max()) {
-							continue;
-						}
-						if (v == u) {
-							Lv_final.push_back(Lv[i]);
-							T[Lv[i].vertex] = Lv[i].distance;
-							continue;
-						}
-						mtx_595[u].lock();
-						auto Lu = L[u];
-						mtx_595[u].unlock();
+				int size = Lv.size();
+				for (int i = 0; i < size; i++) {
+					int u = Lv[i].vertex;
+					if (Lv[i].distance == std::numeric_limits<weightTYPE>::max()) {
+						continue;
+					}
+					if (v == u) {
+						Lv_final.push_back(Lv[i]);
+						T[Lv[i].vertex] = Lv[i].distance;
+						continue;
+					}
+					mtx_595[u].lock();
+					auto Lu = L[u];
+					mtx_595[u].unlock();
 
-						double min_dis = std::numeric_limits<weightTYPE>::max();
-						int min_dis_v;
-						for (auto label : Lu) {
-							double query_dis = label.distance + T[label.vertex];
-							if (query_dis < min_dis) {
-								min_dis = query_dis;
-								min_dis_v = label.vertex;
-							}
-						}
-
-						if (min_dis > Lv[i].distance + 1e-5) {
-							Lv_final.push_back(Lv[i]);
-							T[Lv[i].vertex] = Lv[i].distance;
-						}
-						else {
-							if (min_dis_v != u) {
-								mtx_5952[v].lock();
-								PPR_insert(PPR, v, min_dis_v, u);
-								mtx_5952[v].unlock();
-							}
-							if (min_dis_v != v) {
-								mtx_5952[u].lock();
-								PPR_insert(PPR, u, min_dis_v, v);
-								mtx_5952[u].unlock();
-							}
+					double min_dis = std::numeric_limits<weightTYPE>::max();
+					int min_dis_v;
+					for (auto label : Lu) {
+						double query_dis = label.distance + T[label.vertex];
+						if (query_dis < min_dis) {
+							min_dis = query_dis;
+							min_dis_v = label.vertex;
 						}
 					}
 
-					for (auto label : Lv_final) {
-						T[label.vertex] = std::numeric_limits<weightTYPE>::max();
+					if (min_dis > Lv[i].distance + 1e-5) {
+						Lv_final.push_back(Lv[i]);
+						T[Lv[i].vertex] = Lv[i].distance;
 					}
-
-					mtx_595[v].lock();
-					vector<two_hop_label_v1>(Lv_final).swap(L[v]);
-					mtx_595[v].unlock();
-
+					else {
+						if (min_dis_v != u) {
+							mtx_5952[v].lock();
+							PPR_insert(PPR, v, min_dis_v, u);
+							mtx_5952[v].unlock();
+						}
+						if (min_dis_v != v) {
+							mtx_5952[u].lock();
+							PPR_insert(PPR, u, min_dis_v, v);
+							mtx_5952[u].unlock();
+						}
+					}
 				}
+
+				for (auto label : Lv_final) {
+					T[label.vertex] = std::numeric_limits<weightTYPE>::max();
+				}
+
+				mtx_595[v].lock();
+				vector<two_hop_label_v1>(Lv_final).swap(L[v]);
+				mtx_595[v].unlock();
+
+				mtx_595[max_N_ID_for_mtx_595 - 1].lock();
+				Qid_clean_PPR.push(used_id);
+				mtx_595[max_N_ID_for_mtx_595 - 1].unlock();
 
 				return 1; // return to results; the return type must be the same with results
 				}));
@@ -112,7 +120,6 @@ it is slower to use thread_vertices_clean_L_dynamic in the following codes
 vector<vector<weightTYPE>> d_clean_PPR;
 vector<vector<weightTYPE>> T_clean_PPR;
 vector<vector<graph_hash_of_mixed_weighted_HL_PLL_v1_handle_t_for_sp>> Q_handles_clean_PPR;
-queue<int> Qid_clean_PPR;
 
 void clean_PPR(graph_v_of_v_idealID& ideal_g, vector<vector<two_hop_label_v1>>& L, std::vector<std::vector<std::pair<int, std::vector<int>>>>& PPR,
 	ThreadPool& pool, std::vector<std::future<int>>& results, int thread_num) {
